5ae1cdd1a065e434c8a50e2e16b9854a2afc70b0
abhikmp <abhikampurath@gmail.com>
2020-11-23 20:01:36 +0530

    added backend for login & register and connected db

---
+++ home-temp.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>home1</title>
+</head>
+
+<body>
+
+
+    <?php if (isset($_SESSION['username'])) : ?>
+        <h3>Welcome <?php print($_SESSION['username']); ?></h3>
+    <?php endif ?>
+
+</body>
+
+</html>
