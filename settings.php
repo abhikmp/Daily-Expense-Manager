bff27736663bc442f1489b5dcc9c425877c86bd5
mushrifshahreyar <mushrifshahreyar@gmail.com>
2020-11-26 20:00:40 +0530

    minor bug fixes

--- settings.php
+++ settings.php
@@ -1,19 +1,23 @@
+<!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
         <link rel="stylesheet" type="text/css" href="settings.css">
     </head>
 
     <body>
-        <div class="navbar navbar-expand-lg navbar-dark bg-dark navbar-fixed-top navbar-header">
-            <a class="navbar-brand" >Daily Expense Manager</a>
-        </div>
-
-
-    </style>
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            
+            <a  class="navbar-brand" href="dashboard.php">Daily Expense Manager</a>
+        </nav>
+        
+    
         
         <div class="container container-form">
             <div class="card-body">
@@ -58,8 +62,9 @@
             </div>
             </form>
         </div>
-
         </div>
+
+        
     </body>
 
 
